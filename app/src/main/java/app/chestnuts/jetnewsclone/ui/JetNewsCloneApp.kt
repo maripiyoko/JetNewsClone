d25f830f977d2d983df2ae8324aa0be65c24d6ed
maripiyo <maripiyok@gmail.com>
2024-06-29 10:04:50 +0900

    Add color theme.

--- app/src/main/java/app/chestnuts/jetnewsclone/ui/JetNewsCloneApp.kt
+++ app/src/main/java/app/chestnuts/jetnewsclone/ui/JetNewsCloneApp.kt
@@ -1,15 +1,26 @@
 package app.chestnuts.jetnewsclone.ui
 
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import app.chestnuts.jetnewsclone.data.AppContainer
+import app.chestnuts.jetnewsclone.ui.theme.JetNewsCloneAppTheme
 
 @Composable
 fun JetNewsCloneApp(
     appContainer: AppContainer,
 ) {
-    Column {
-        Text(text = "compose app")
+    JetNewsCloneAppTheme {
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier.fillMaxSize()
+                //.border(1.dp, Color.Red)
+                //.background(Color.Yellow)
+        ) {
+            Text(text = "compose app")
+        }
     }
 }
---
+++ app/src/main/java/app/chestnuts/jetnewsclone/ui/theme/Type.kt
@@ -0,0 +1,5 @@
+package app.chestnuts.jetnewsclone.ui.theme
+
+import androidx.compose.material3.Typography
+
+val JetNewsCloneAppTypography = Typography()
